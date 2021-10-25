79a2c65947a809d41145a4f970094b2f6481323e
CaoBinh18 <binhcao18@gmail.com>
2021-10-25 09:23:03 +0700

    i18n

---
+++ JQuery-Ajax/phone-management/src/main/java/com/cg/service/SmartphoneService.java
@@ -0,0 +1,34 @@
+package com.cg.service;
+
+import com.cg.model.Smartphone;
+import com.cg.repository.ISmartphoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SmartphoneService implements ISmartphoneService {
+    @Autowired
+    private ISmartphoneRepository smartPhoneRepository;
+
+    @Override
+    public Iterable<Smartphone> findAll() {
+        return smartPhoneRepository.findAll();
+    }
+
+    @Override
+    public Optional<Smartphone> findById(Long id) {
+        return smartPhoneRepository.findById(id);
+    }
+
+    @Override
+    public Smartphone save(Smartphone smartPhone) {
+        return smartPhoneRepository.save(smartPhone);
+    }
+
+    @Override
+    public void remove(Long id) {
+        smartPhoneRepository.deleteById(id);
+    }
+}
